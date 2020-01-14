a50d6f6460261e7709b09ee0ac6e29c9ba4fa69a
Roman Uskov <romanuskov94@gmail.com>
2020-01-14 21:46:51 +0300

    work is done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from './modules/sum';
+import guard from './modules/guard';
+
+console.log(sum(1,22));
+console.log(guard("Roma"));
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+export default function (name) {
+  if (name == "Roma") {
+    return "Hello, Roma! Please, come in";
+  } else {
+    return "Get out!";
+  }
+};
